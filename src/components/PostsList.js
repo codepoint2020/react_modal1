29feaa134f2e27c3ae304e6945dfce465314b780
j-morales-code <jeromemorales86@live.com>
2023-03-23 07:44:38 +0800

    Created new branch

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -5,7 +5,28 @@ import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 function PostsList({isPosting, onStopPosting}) {
-    
+
+    const [myData, setMyData] = useState([
+        {
+            id: 1,
+            author: "Jerome Morales",
+            body: "Just want to get rich and get laid to lovely, sexy and hot women of all races on earth." 
+        },
+
+        {
+            id: 2,
+            author: "Elon Musk",
+            body: "I dream of us becoming a space faring civilization" 
+        },
+
+        {
+            id: 3,
+            author: "Mark Zuckerberg",
+            body: "I revolutionized social media." 
+        }
+
+    ]);
+
     const [ enteredBody, setEnteredBody ] = useState('');
     const [ enteredAuthor, setEnteredAuthor ] = useState('');
 
@@ -31,7 +52,8 @@ function PostsList({isPosting, onStopPosting}) {
         }
         
             <ul className={classes.posts}>
-                <Post author={enteredAuthor} body={enteredBody}/>
+                {}
+                <Post author={"author"} body={"body"}/>
             </ul>
         </>
     )
